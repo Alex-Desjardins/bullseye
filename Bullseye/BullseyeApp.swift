90fafd41b95baef92b72e8d92b9d3ffc4f72471c
Alex Desjardins <ardesjardins91@gmail.com>
2022-05-12 10:53:41 -0600

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Alex Desjardins on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
